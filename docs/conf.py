a5af055495aa72cbe7ee2067a9fe912ef0f18f95
fjaviersanchez <francs1@uci.edu>
2019-04-07 15:35:44 -0700

    Fixed conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc', 'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -111,7 +111,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
