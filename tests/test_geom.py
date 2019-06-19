8ece36f079cb870a798657befce6a9505dd5a997
Erin Sheldon <erin.sheldon@gmail.com>
2019-06-19 10:06:29 -0400

    add circle test

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -2,6 +2,147 @@ import numpy as np
 import healsparse
 
 
+def test_circle_smoke():
+    """
+    just test we can make a circle and a map from it
+    """
+    ra, dec = 200.0, 0.0
+    radius = 30.0/3600.0
+    nside = 2**17
+    circle = healsparse.Circle(
+        ra=ra,
+        dec=dec,
+        radius=radius,
+        value=2**4,
+    )
+    pixels = circle.get_pixels(nside=nside)  # noqa
+
+    smap = circle.get_map(nside=nside, dtype=np.int16)  # noqa
+
+
+def _randcap(rng, nrand, ra, dec, rad, get_radius=False):
+    """
+    Generate random points in a sherical cap
+
+    parameters
+    ----------
+
+    nrand:
+        The number of random points
+    ra,dec:
+        The center of the cap in degrees.  The ra should be within [0,360) and
+        dec from [-90,90]
+    rad:
+        radius of the cap, same units as ra,dec
+
+    get_radius: bool, optional
+        if true, return radius of each point in radians
+    """
+    # generate uniformly in r**2
+    rand_r = rng.uniform(size=nrand)
+    rand_r = np.sqrt(rand_r)*rad
+
+    # put in degrees
+    np.deg2rad(rand_r, rand_r)
+
+    # generate position angle uniformly 0,2*PI
+    rand_posangle = rng.uniform(nrand)*2*np.pi
+
+    theta = np.array(dec, dtype='f8', ndmin=1, copy=True)
+    phi = np.array(ra, dtype='f8', ndmin=1, copy=True)
+    theta += 90
+
+    np.deg2rad(theta, theta)
+    np.deg2rad(phi, phi)
+
+    sintheta = np.sin(theta)
+    costheta = np.cos(theta)
+    # sinphi = np.sin(phi)
+    # cosphi = np.cos(phi)
+
+    sinr = np.sin(rand_r)
+    cosr = np.cos(rand_r)
+
+    cospsi = np.cos(rand_posangle)
+    costheta2 = costheta*cosr + sintheta*sinr*cospsi
+
+    np.clip(costheta2, -1, 1, costheta2)
+
+    # gives [0,pi)
+    theta2 = np.arccos(costheta2)
+    sintheta2 = np.sin(theta2)
+
+    cosDphi = (cosr - costheta*costheta2)/(sintheta*sintheta2)
+
+    np.clip(cosDphi, -1, 1, cosDphi)
+    Dphi = np.arccos(cosDphi)
+
+    # note fancy usage of where
+    phi2 = np.where(rand_posangle > np.pi, phi+Dphi, phi-Dphi)
+
+    np.rad2deg(phi2, phi2)
+    np.rad2deg(theta2, theta2)
+    rand_ra = phi2
+    rand_dec = theta2-90.0
+
+    # atbound(rand_ra, 0.0, 360.0)
+
+    if get_radius:
+        np.rad2deg(rand_r, rand_r)
+        return rand_ra, rand_dec, rand_r
+    else:
+        return rand_ra, rand_dec
+
+
+def test_circle_values():
+    """
+    make sure we get out the value we used for the map
+
+    Note however that we do not use inclusive intersections, we we will test
+    values from a slightly smaller circle
+    """
+
+    rng = np.random.RandomState(7812)
+    nside = 2**17
+
+    ra, dec = 200.0, 0.0
+    radius = 30.0/3600.0
+    circle = healsparse.Circle(
+        ra=ra,
+        dec=dec,
+        radius=radius,
+        value=2**4,
+    )
+
+    smap = circle.get_map(nside=nside, dtype=np.int16)
+
+    # test points we expect to be inside
+    smallrad = radius*0.95
+    nrand = 10000
+    rra, rdec = _randcap(rng, nrand, ra, dec, smallrad)
+
+    vals = smap.getValueRaDec(rra, rdec)  # noqa
+
+    assert np.all(vals == circle.value)
+
+    # test points we expect to be outside
+    bigrad = radius*2
+    nrand = 10000
+    rra, rdec, rrand = _randcap(
+        rng,
+        nrand,
+        ra,
+        dec,
+        smallrad,
+        get_radius=True,
+    )
+    w, = np.where(rrand > 1.1*radius)
+
+    vals = smap.getValueRaDec(rra[w], rdec[w])  # noqa
+
+    assert np.all(vals == 0)
+
+
 def test_polygon_smoke():
     """
     just test we can make a polygon and a map from it
