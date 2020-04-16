8ac7b056022999c2979b28760f3db8ce9bad704f
Eli Rykoff <erykoff@stanford.edu>
2020-04-16 10:46:55 -0700

    Add install_requires for proper pip installation

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     name=name,
     packages=find_packages(exclude=('tests')),
     version=__version__, # noqa
-    description='A sparse healpix implementation',
+    description='Sparse healpix maps and geometry library',
     author='Eli Rykoff and others',
     author_email='erykoff@stanford.edu',
     url='https://github.com/lsstdesc/healsparse',
+    install_requires=['numpy', 'healpy'],
 )
