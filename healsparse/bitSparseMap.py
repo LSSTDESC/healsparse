0b12a7fab6b97c68561dcb1d4f5e14e0f59cdd39
Eli Rykoff <erykoff@stanford.edu>
2023-11-06 16:49:50 -0800

    Rename and improve BitSparseMap

--- healsparse/bitSparseMap.py
+++ healsparse/bitSparseMap.py
@@ -13,15 +13,14 @@ class BitSparseMap:
             if not isinstance(data_buffer, np.ndarray) or data_buffer.dtype != np.uint8:
                 raise ValueError("data_buffer must be a numpy array of type uint8")
 
-            self._size = len(data_buffer) * 8
             self._data = data_buffer
         else:
             # Check if size is multiple of 8.
             if (size % 8) != 0:
                 raise ValueError("BitSparseMap must have a size that is a multiple of 8.")
 
-            self._size = size
             self._data = np.zeros(size // 8, dtype=np.uint8)
+            # We need to rething this part.
             if fill_value:
                 # Set to all 1s
                 self._data[:] = 255
@@ -41,22 +40,17 @@ class BitSparseMap:
 
     @property
     def shape(self):
-        return (self._size, )
+        return (self.size, )
 
     @property
     def size(self):
-        return self._size
+        return len(self._data) * 8
 
     def resize(self, newsize, refcheck=False):
         if (newsize % 8) != 0:
             raise ValueError("BitSparseMap must have a size that is a multiple of 8.")
 
         self._data.resize(newsize // 8, refcheck=refcheck)
-        self._size = newsize
-
-    def reshape(self, newshape):
-        # This is a tricky one.
-        pass
 
     def sum(self, shape=None, axis=None):
         if shape is None:
@@ -68,7 +62,7 @@ class BitSparseMap:
                 raise ValueError(f"Axis {axis} is out of bounds for shape.")
             # The shape needs to (a) the last axis must be a multiple
             # of 8; the product needs to equal ths size.
-            if np.prod(shape) != self._size:
+            if np.prod(shape) != self.size:
                 raise ValueError("Shape mismatch with array size.")
             if shape[-1] % 8 != 0:
                 raise ValueError("Final shape index must be a multiple of 8.")
@@ -82,14 +76,23 @@ class BitSparseMap:
     def data_array(self):
         return self._data
 
+    def copy(self):
+        """Return a copy.
+
+        Returns
+        -------
+        copy : `BitSparseMap`
+        """
+        return BitSparseMap(data_buffer=self._data.copy())
+
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "FIXME"
+        return f"BitSparseMap(size={self.size})"
 
     def __len__(self):
-        return self._size
+        return self.size
 
     def __getitem__(self, key):
         # If it's a slice, we return a BitSparseMap view.
@@ -115,17 +118,17 @@ class BitSparseMap:
 
             return BitSparseMap(data_buffer=self._data[slice(start, stop, step)])
         elif isinstance(key, numbers.Integral):
-            return self._test_bits(self._data, key)[0]
+            return self._test_bits_at_locs(key)[0]
         elif isinstance(key, np.ndarray):
             if not is_integer_value(key.dtype.type(0)):
                 raise IndexError("Numpy array indices must be integers for __getitem__")
-            return self._test_bits(self._data, key)
+            return self._test_bits_at_locs(key)
         elif isinstance(key, (list, tuple)):
             arr = np.atleast_1d(key)
             if len(arr) > 0:
                 if not is_integer_value(arr[0]):
                     raise IndexError("List array indices must be integers for __getitem__")
-            return self._test_bits(self._data, arr)
+            return self._test_bits_at_locs(arr)
         else:
             raise IndexError("Illegal index type (%s) for __getitem__ in BitSparseMap." %
                              (key.__class__))
@@ -137,43 +140,81 @@ class BitSparseMap:
         if isinstance(key, numbers.Integral):
             # Need to check that value is single-valued; and bool.
             if value:
-                return self._set_bits(self._data, key)
+                return self._set_bits_at_locs(key)
             else:
-                return self._clear_bits(self._data, key)
+                return self._clear_bits_at_locs(key)
         elif isinstance(key, slice):
-            start = key.start if key.start is not None else 0
-            stop = key.stop if key.stop is not None else self._size
-            step = key.step if key.step is not None else 1
-
-            # In principle it's possible to use a slice with the bits,
-            # but not with the div and mod operations unless it's a
-            # factor of 8.  So we'd have to have two different
-            # code paths I fear.  Unsure if it would be useful.
-            indices = np.arange(start, stop, step)
-
-            # Need to check that value is single valued bool or array of bool with
-            # the right length.
-            if isinstance(value, (bool, np.bool_)):
-                if value:
-                    return self._set_bits(self._data, indices)
+            slice8 = True
+            if key.start is not None:
+                if key.start % 8 != 0:
+                    slice8 = False
                 else:
-                    return self._clear_bits(self._data, indices)
-
-            elif isinstance(value, np.ndarray):
-                if value.dtype != self._dtype:
-                    raise ValueError("Can only set to array of bools")
-                if len(value) != len(indices):
-                    raise ValueError("Length mismatch")
-                self._set_bits(self._data, indices[value])
-                self._clear_bits(self._data, indices[~value])
+                    start = key.start // 8
             else:
-                raise ValueError("Can only set to bool or array of bools")
+                start = None
+            if key.stop is not None:
+                if key.stop % 8 != 0:
+                    slice8 = False
+                else:
+                    stop = key.stop // 8
+            else:
+                stop = None
+            if key.step is not None:
+                if key.step % 8 != 0:
+                    slice8 = False
+                else:
+                    step = key.step // 8
+            else:
+                step = None
+
+            if slice8:
+                # We can do optimized operations here.
+                s8 = slice(start, stop, step)
+
+                if isinstance(value, (bool, np.bool_)):
+                    if value:
+                        # This is all True.
+                        self._data[s8] = np.uint8(-1)
+                    else:
+                        # This is all False.
+                        self._data[s8] = np.uint8(0)
+                elif isinstance(value, np.ndarray):
+                    if value.dtype != self._dtype:
+                        raise ValueError("Can only set to array of bools")
+                    if len(range(*s8.indices(len(self._data)))) != value.size // 8:
+                        raise ValueError("Length of values does not match slice.")
+                    self._data[s8] = np.packbits(value, bitorder="little")
+            else:
+                # Unoptimized operations
+                start = key.start if key.start is not None else 0
+                stop = key.stop if key.stop is not None else self.size
+                step = key.step if key.step is not None else 1
+
+                indices = np.arange(start, stop, step)
+
+                # Need to check that value is single valued bool or array of bool with
+                # the right length.
+                if isinstance(value, (bool, np.bool_)):
+                    if value:
+                        return self._set_bits_at_locs(indices)
+                    else:
+                        return self._clear_bits_at_locs(indices)
+
+                elif isinstance(value, np.ndarray):
+                    if value.dtype != self._dtype:
+                        raise ValueError("Can only set to array of bools")
+                    if len(value) != len(indices):
+                        raise ValueError("Length mismatch")
+                    self._set_bits_at_locs(indices[value])
+                    self._clear_bits_at_locs(indices[~value])
+                else:
+                    raise ValueError("Can only set to bool or array of bools")
         elif isinstance(key, np.ndarray):
             if not is_integer_value(key.dtype.type(0)):
                 raise IndexError("Numpy array indices must be integers for __setitem__")
             if isinstance(value, (bool, np.bool_)):
                 if value:
-                    return self._set_bits(self._data, key)
+                    return self._set_bits_at_locs(key)
                 else:
                     return self._clear_bits(self._data, key)
             elif isinstance(value, np.ndarray):
@@ -181,27 +222,27 @@ class BitSparseMap:
                     raise ValueError("Can only set to bool or array of bools")
                 if len(value) != len(key):
                     raise ValueError("Length mismatch")
-                self._set_bits(self._data, key[value])
-                self._clear_bits(self._data, key[~value])
+                self._set_bits_at_locs(key[value])
+                self._clear_bits_at_locs(key[~value])
             else:
                 raise ValueError("Can only set to bool or array of bools")
-        elif isinstance(key, list):
+        elif isinstance(key, (list, tuple)):
             indices = np.atleast_1d(key)
             if len(indices) > 0:
                 if not is_integer_value(indices[0]):
                     raise IndexError("List array indices must be integers for __setitem__")
             if isinstance(value, (bool, np.bool_)):
                 if value:
-                    return self._set_bits(self._data, indices)
+                    return self._set_bits_at_locs(indices)
                 else:
-                    return self._clear_bits(self._data, indices)
+                    return self._clear_bits_at_locs(indices)
             elif isinstance(value, np.ndarray):
                 if value.dtype != self._dtype:
                     raise ValueError("Can only set to bool or array of bools")
                 if len(value) != len(key):
                     raise ValueError("Length mismatch")
-                self._set_bits(self._data, indices[value])
-                self._clear_bits(self._data, indices[~value])
+                self._set_bits_at_locs(indices[value])
+                self._clear_bits_at_locs(indices[~value])
             else:
                 raise ValueError("Can only set to bool or array of bools")
         else:
@@ -209,30 +250,49 @@ class BitSparseMap:
                              (key.__class__))
 
     def __array__(self):
-        return self._test_bits(self._data, np.arange(0, self._size))
+        # Can I avoid this arange?
+        return self._test_bits_at_locs(np.arange(0, self.size))
 
-    def _set_bits(self, array, locs):
+    def __and__(self, other):
+        pass
+
+    def __iand__(self, other):
+        pass
+
+    def __or__(self, other):
+        pass
+
+    def __ior__(self, other):
+        pass
+
+    def __xor__(self, other):
+        pass
+
+    def __ixor__(self, other):
+        pass
+
+    def _set_bits_at_locs(self, locs):
         _locs = np.atleast_1d(locs)
 
         np.bitwise_or.at(
-            array,
+            self._data,
             _locs // 8,
             1 << (_locs % 8).astype(np.uint8),
         )
 
-    def _clear_bits(self, array, locs):
+    def _clear_bits_at_locs(self, locs):
         _locs = np.atleast_1d(locs)
 
         np.bitwise_and.at(
-            array,
+            self._data,
             _locs // 8,
             ~(1 << (_locs % 8).astype(np.uint8)),
         )
 
-    def _test_bits(self, array, locs):
+    def _test_bits_at_locs(self, locs):
         _locs = np.atleast_1d(locs)
 
-        return array[_locs // 8] & (1 << (_locs % 8).astype(np.uint8)) != 0
+        return self._data[_locs // 8] & (1 << (_locs % 8).astype(np.uint8)) != 0
 
     def _bit_count(self, arr):
         arr = arr - ((arr >> 1) & self._s55)
