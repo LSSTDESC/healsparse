959aedd70b5435e35a58262f50d22a2ab93d1593
elvinpoole <jack.elvinpoole@gmail.com>
2026-01-20 13:09:19 -0500

    fixed test bugs in hdf5 function

--- healsparse/io_map.py
+++ healsparse/io_map.py
@@ -56,7 +56,7 @@ def _read_map(healsparse_class, filename, nside_coverage=None, pixels=None, head
         fits = HealSparseFits(filename)
         is_fits_file = True
         fits.close()
-    except OSError:
+    except (OSError, UnicodeDecodeError):
         pass
 
     if not is_fits_file:
@@ -76,7 +76,7 @@ def _read_map(healsparse_class, filename, nside_coverage=None, pixels=None, head
                                  use_threads=use_threads)
     elif is_hdf5_file:
         # TODO: pass pixels, degrade options, etc to hdf5 read
-        return _read_map_hdf5(healsparse_class, filename, hdf5_group=hdf5_group,
+        return _read_map_hdf5(healsparse_class, filename, group=hdf5_group,
                               pixels=pixels, header=header, degrade_nside=degrade_nside,
                               weightfile=weightfile, reduction=reduction,)
     elif not os.path.isfile(filename):
